c1293d1aa495805d123c250751accb92d911c6f7
KerstenBreuer <kersten-breuer@outlook.com>
2021-07-12 06:52:13 +0000

    add basic layout

---
+++ src/components/Main.tsx
@@ -0,0 +1,13 @@
+import TopBar from "./TopBar";
+
+const Main = () => (
+    <div>
+        <TopBar></TopBar>
+        <div>
+            Welcome to the GHGA Data Portal.
+        </div>
+
+    </div>
+);
+
+export default Main;
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,9 @@
+const TopBar = () => (
+    <div className="w3-card w3-black">
+        <div className="w3-button w3-green">GHGA DataPortal</div>
+        <div className="w3-bar-item w3-button">Browse</div>
+        <div className="w3-bar-item w3-button">My Datasets</div>
+    </div>
+);
+
+export default TopBar;
