53f8b139a7255f082ed63745a9ebb239eea77d41
KerstenBreuer <kersten-breuer@outlook.com>
2021-07-30 12:58:28 +0000

    add react router dom

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import TopBar from './TopBar';
 import Welcome from '../components/Welcome';
 import NotImplementedYet from '../components/NotImplementedYet';
