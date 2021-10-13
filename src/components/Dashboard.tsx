1bdd13ca781e4113e0c6467ca30e2c19da51d6dd
zehrahazalsezer <hazal.sezer@live.acibadem.edu.tr>
2021-10-13 09:05:03 +0000

    updated dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,52 +1,97 @@
+import React, { useState } from 'react';
 import logoFull from '../logo_full.png';
 import Message from './Message';
 import { datasetModel } from '../dataModels/metadata';
-import DataSetList from './Explore/DatasetList';
 import { getAllDatasets } from "../backendCalls/metadata";
-import React from 'react';
-
+import { requestModel } from "../dataModels/requests";
+import { getAllRequests } from '../backendCalls/requests';
+import { users } from "../data/users";
 
 const DashboardModule = () =>{
     const [dsList, setDsList] = React.useState<datasetModel[]|null>(null);
     React.useEffect( () => getAllDatasets(setDsList), []);
     var num_ds= dsList?.length;
+
+    const [reqList, setReqList] = React.useState<requestModel[]|null>(null);
+    React.useEffect( () => getAllRequests(setReqList), []);
+    const reqCarryList = (reqList !== null) ? reqList.filter( (req) => req.user_id === "USR:0000001") : [];
+    var num_reqs_carry = reqCarryList.length;
+
+    const RequestApprove =  (reqList !== null) ? reqList.filter( (req) => req.status === "approved") : [];
+    var num_reqs_approved= RequestApprove.length;
+
     
+    var num_user = users.length;
+   
+    const [dateState] = useState(new Date());
+
     return (
 
     <div className="w3-panel w3-center">
-        <div  className="dash_panels">
-            <img src= {logoFull} alt="" width= "200px" height="auto"/>
-            <h2 style={{color: "black"}}>GHGA Sandbox</h2>
-
-            <p style={{color: "black", fontSize: "10px"}}>
-                Explore. Exract. Experiment. Enjoy.
+        <div style= {{width: "auto", height: "600px"}}
+        className="dash_panels">
+            <br /><br /><br /><img src= {logoFull} alt="" width= "200px" height="auto"/><br /><br /><br />
+            <h3 style={{color: "black", position: "inherit", textAlign: "center"}}>GHGA Sandbox</h3><br /><br /><br />
+            <p style={{color: "black", fontSize: "20px",position: "inherit" }}>
+                Explore. <br />Extract.<br />Experiment.<br />Enjoy.
             </p>
         </div>
       <div className="dash_panels">
-            <h1 style={{color: "black"}} >
-            &#129516;
-               Total Datasets {num_ds}
+            <h1 style={{color: "black", fontSize: "20px"}} >
+            <span style={{fontSize: "25px"}} >&#129516;</span>
+               Total Datasets: 
+               <p style= {{ fontSize: "60px", position: "inherit" }}> {num_ds} </p> 
 
             </h1>
             
       </div>
-      <div className="dash_panels">
-     
-      </div>
-      <div className="dash_panels">
       
-      </div>
       <div className="dash_panels">
-      
+      <h1 style={{color: "black", fontSize: "20px"}} >
+        Total Requests by Carry:
+        <p style= {{ fontSize: "60px", position: "inherit", fontOpticalSizing:"auto" }}> {num_reqs_carry} </p> 
+        </h1>
       </div>
-      <div className="dash_panels">
       
+      <div className="dash_panels">
+      <h1 style={{color: "black", fontSize: "20px"}} >
+        Total Request Approved:
+      <p style= {{ fontSize: "60px", position: "inherit" }}> {num_reqs_approved} </p> 
+      </h1>
       </div>
+
       <div className="dash_panels">
-      
+      <h1 style={{color: "black", fontSize: "20px"}} >
+        Total Number of User:
+        <p style= {{ fontSize: "60px", position: "inherit" }}> {num_user} </p> 
+        </h1>
       </div>
+
       <div className="dash_panels">
+      <h1 style={{color: "black", fontSize: "20px"}} >
+        Total Number of File:
+        </h1>
+      </div>
       
+      <div className="dash_panels">
+        <span style={{fontSize: "30px"}} >&#128197;</span>
+        <p style={{fontSize: "20px"}}>
+              {' '}
+              {dateState.toLocaleDateString('en-GB', {
+                 day: 'numeric',
+                 month: 'short',
+                 year: 'numeric',
+              })}
+            </p>
+            	
+        <span style={{fontSize: "30px"}} >&#128337;</span>
+        <p style={{fontSize: "20px"}}>
+             {dateState.toLocaleString('en-US', {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true,
+            })}
+            </p>
       </div>
       {process.env.REACT_APP_WELCOME_INFO && (
             <div style={{ marginTop: "50px", clear: "both" }}>
